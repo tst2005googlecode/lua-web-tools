3648b8a1ed853757cc26b32b84d029d1e8c6b183
andre@naef.com <andre@naef.com@403eb6ef-2ecc-4d33-972f-a1a85230eea8>
2011-04-07 16:35:55 +0000

    Reworked template engine to use FILE pointers for output. Improved Lua httpd.write_template() function to optionally accept file pointers and return the output as a string. Added APR table iteration to the Lua httpd.dump() function

--- trunk/mod_lwt/template.h
+++ trunk/mod_lwt/template.h
@@ -19,42 +19,44 @@ void lwt_template_init (apr_pool_t *pool);
 /**
  * Prepares a template for rendering.
  *
- * @param r the request record
  * @param filename the file name
+ * @param L the Lua state
  * @param flags the flags
+ * @param pool a pool for allocations
  * @param t is assigned the parsed template on success (unless NULL)
  * @param err is assigned the error message in case of an error (unless NULL)
  * @return APR_SUCCESS if the template is successfully rendered, and an error
  * status otherwise
  */
-apr_status_t lwt_template_parse (request_rec *r, lua_State *L,
-		const char *filename, const char *flags,
-		apr_array_header_t **t, const char **err);
+apr_status_t lwt_template_parse (const char *filename, lua_State *L,
+		const char *flags, apr_pool_t *pool, apr_array_header_t **t,
+		const char **err);
 
 /**
  * Renders a prepared template.
  *
- * @param r the request record
- * @param L the Lua state
  * @param t the prepared template
+ * @param L the Lua state
+ * @param pool a pool for allocations
+ * @param f the output file pointer
  * @param err is assigned the error message in case of an error (unless NULL)
  * @return APR_SUCCESS if the template is successfully rendered, and an error
  * status otherwise
  */
-apr_status_t lwt_template_render (request_rec *r, lua_State *L,
-		apr_array_header_t *t, const char **err);
+apr_status_t lwt_template_render (apr_array_header_t *t, lua_State *L,
+		apr_pool_t *pool, FILE *f, const char **err);
 
 /**
  * Dumps a prepared template.
  *
- * @param r the request record
- * @param L the Lua state
  * @param t the prepared template
+ * @param L the Lua state
+ * @param f the output file pointer
  * @param err is assigned the error message in case of an error (unless NULL)
  * @return APR_SUCCESS if the template is successfully dumped, and an error
  * status otherwise
  */
-apr_status_t lwt_template_dump (request_rec *r, lua_State *L,
-		apr_array_header_t *t, const char **err);
+apr_status_t lwt_template_dump (apr_array_header_t *t, lua_State *L, FILE *f,
+		const char **err);
 
 #endif /* MOD_LWT_TEMPLATE_INCLUDED */
